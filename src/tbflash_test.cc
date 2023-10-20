820e01ad6206b377df2a33faee904a3a5e4c28b7
WesleyCh3n <wesley.ch3n.0530@gmail.com>
2023-10-21 00:16:36 +0800

    update: mock time

--- src/tbflash_test.cc
+++ src/tbflash_test.cc
@@ -4,5 +4,6 @@
 int main(int argc, char *argv[]) {
   std::vector<char *> args(argv, argv + argc);
   spdlog::info("Got argv: {}", args);
+  std::this_thread::sleep_for(std::chrono::seconds(5));
   return 0;
 }
